696f536f0724de9a63a736dedb828af54f694450
ludele <Luddesnek@proton.me>
2023-11-24 14:13:05 +0100

    update

--- routing/index.js
+++ routing/index.js
@@ -1,18 +1,23 @@
-const http = require("http")
-const port = 3000
-const rootHandler = require("./rootHandler")
+const http = require("http");
+const port = 3000;
+const rootHandler = require("./rootHandler");
 
-async function handleRequest(request, response){
-   let url = new URL(request.url, "http://" + request.headers.host)
-   let path = url.pathname
-   let pathSegments = path.split("/").filter(function(element){
-      return element !== ""
-   })
-   console.log(pathSegments)
-   rootHandler.handleRoute(pathSegments, request, response)
+  let url = new URL(request.url, "http://" + request.headers.host);
+
+  let path = url.pathname;
+
+  let pathSegments = path.split("/").filter(function (element) {
+    return element !== "";
+  });
+
+  console.log(pathSegments);
+
+  rootHandler.handleRoute(pathSegments, request, response);
 }
 
-let app = http.createServer(handleRequest)
+let app = http.createServer(handleRequest);
 
-app.listen(port)
-console.log(`Server listening on port ${port}`)
+const localIP = '10.156.32.188'; // Replace with your actual local IP address
+app.listen(port, localIP);
+
+console.log(`Server listening on http://${localIP}:${port}/`);
